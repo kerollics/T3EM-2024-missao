a5f7b4ca1e51ea45db01562c63ea6efd1d8023f9
kerolli <112178863+kerollics@users.noreply.github.com>
2024-06-18 11:03:26 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlterantivas = document.querySelector(".caixa-alterantiva");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "Pergunta 1,
+    alternativas: [
+        "Alternativa 1",
+        " Alternativa 2"
+        ]
+    ];`
+   
+   enunciado: "Pergunta 2",
+   alternativas: [
+    "Alternativas 1",
+    "Alternativas 2"
+   ]
+}
+];
